1b0a2c6fecd2960d0eeabc510c69d3ffccd8dcda
Adam Tassara Powell <apowell@umass.edu>
2022-11-28 11:55:17 -0500

    Move get-info into subcommand

--- main.py
+++ main.py
@@ -17,20 +17,7 @@ Add subcommand for outputting an anki deck with given parameters.
 Organize utils
 """
 
-def get_bird(bird_id: str):
-    bird_info = get_bird_info(bird_id)
-    save_bird_info(bird_info)
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog = "bird-guide-downloader"
-    )
-    parser.add_argument('-r', '--region',
-        choices = region_to_tid_dict.keys(),
-    )
-
-    args = parser.parse_args()
-
+def subcommand_get_info(args):
     if args.region != None:
         region_tid = region_id_to_tid(args.region)
     else:
@@ -42,3 +29,24 @@ if __name__ == "__main__":
 
     print("Loading bird info")
     Pool(processes = 8).map(get_bird, bird_ids)
+
+def get_bird(bird_id: str):
+    bird_info = get_bird_info(bird_id)
+    save_bird_info(bird_info)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog = "bird-guide-downloader"
+    )
+
+    subparsers = parser.add_subparsers(required=True)
+
+    get_info_parser = subparsers.add_parser("get-info", description="Download info about each bird.")
+    get_info_parser.add_argument('-r', '--region',
+        choices = region_to_tid_dict.keys(),
+    )
+    get_info_parser.set_defaults(func=subcommand_get_info)
+
+    # Parse arguments and run the subcommand
+    args = parser.parse_args()
+    args.func(args)
